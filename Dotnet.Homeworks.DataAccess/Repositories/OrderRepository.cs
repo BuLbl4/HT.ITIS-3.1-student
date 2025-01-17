3aac1a59d5150eaae4b873bf9c867608e5eccedd
BuLbl4 <113981565+BuLbl4@users.noreply.github.com>
2025-01-17 22:00:18 +0300

    fix dotnet.yaml and start workflow

--- Dotnet.Homeworks.DataAccess/Repositories/OrderRepository.cs
+++ Dotnet.Homeworks.DataAccess/Repositories/OrderRepository.cs
@@ -3,6 +3,7 @@ using Dotnet.Homeworks.Domain.Entities;
 
 namespace Dotnet.Homeworks.DataAccess.Repositories;
 
+
 public class OrderRepository : IOrderRepository
 {
     public Task<IEnumerable<Order>> GetAllOrdersFromUserAsync(Guid userId, CancellationToken cancellationToken)
